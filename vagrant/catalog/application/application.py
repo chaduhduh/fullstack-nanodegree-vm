ce930066da170e827b8d38615760e8b457371bc8
chaduhduh <chadd@mainD>
2016-04-04 22:18:49 -0700

    adds working google log in log out

--- vagrant/catalog/application/application.py
+++ vagrant/catalog/application/application.py
@@ -1,3 +1,4 @@
+# imports
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session as login_session, make_response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,31 +12,46 @@ import requests
 import random
 import string
 
+# setup
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = '\x11\xa4W[\xfc\xba\x1df-\xe5OrW\xc1\xc3\xd8>r\xc1\xbciV\xfbp'
-
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
 db = DBSession()
 
+
 # routes
 @app.route('/')
-def Home():
-	return "Welcome"
+def home():
+	return render_template('login.html', data = { "login_session" : login_session })
+
 
 @app.route('/login')
-def showLogin():
+def login():
     hash_key = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['hash_key'] = hash_key
     return render_template('login.html', data = { "login_session" : login_session })
 
+@app.route('/logout')
+def logout():
+	login_session['name'] = ''
+	return redirect(url_for('home'))
+
 @app.route('/connect-googleplus/', methods=['POST'])
 def connect_googleplus():
+	# if request.args.get('hash_key') != login_session['hash_key']:
+	# 	response = make_response(json.dumps('Not Permitted.'), 401)
+	# 	login_session = ''
+	# 	response.headers['Content-Type'] = 'application/json'
+	# 	return response
+	# Obtain authorization code
+	# code = request.data
+	login_session['name'] = "heraldo"
 	return "connecting google plus"
+
 
 @app.route('/update-item/<name>')
 def Update(name):
@@ -48,12 +64,14 @@ def Update(name):
 	else: 
 		return "item not found"
 
+
 @app.route('/delete-item/<name>')
 def Delete(name):
 	item = db.query(Items).filter_by(name=name).first()
 	db.delete(item)
 	db.commit()
 	return "deleted"
+
 
 @app.route('/create-item/<name>')
 def Create(name):
@@ -66,6 +84,7 @@ def Create(name):
 	else:
 		return "nothing added"
 
+
 @app.route('/read-item/<name>')
 def Read(name):
 	item = db.query(Items).filter_by(name=name).first()
@@ -74,6 +93,7 @@ def Read(name):
 	else:
 		return "Nothing to show here"
  	# items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
